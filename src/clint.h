af997b3150cf53d6b1eb0c0e1f81929a05771f57
Paul Loyd <pavelko95@gmail.com>
2014-10-01 12:45:00 +0000

    feat: implement base lexer

---
+++ src/clint.h
@@ -0,0 +1,43 @@
+#ifndef CLINT_H
+#define CLINT_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "tokens.h"
+
+
+enum token_e {
+#define XX(kind, word) kind,
+  TOK_MAP(XX)
+#undef XX
+};
+
+
+typedef struct {
+  enum token_e kind;
+
+  struct {
+    int line;    // 1-indexed
+    int column;  // 1-indexed
+  } start, end;
+} token_t;
+
+
+typedef struct {
+  char *name;
+  char *data;
+  char **lines;  // 1-indexed
+  int nlines;
+} file_t;
+
+
+extern void *xmalloc(size_t size);
+extern void *xrealloc(void *ptr, size_t size);
+
+extern void lex_init(file_t *file);
+extern bool lex_pull(token_t *token);
+extern const char *lex_to_str(token_t *token);
+
+#endif
