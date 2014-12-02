76d0b153b27aebf2c38c4811702976a012b7c48f
Paul Loyd <pavelko95@gmail.com>
2014-12-02 19:14:32 +0000

    feat: "require-line-break" rule

--- rules/lines.c
+++ rules/lines.c
@@ -7,6 +7,8 @@
 static unsigned maximum_length = -1;
 static bool disallow_trailing_space = false;
 static bool require_newline_at_eof = false;
+static const char *line_break = NULL;
+static unsigned line_break_length;
 
 
 static void configure(json_value *config)
@@ -20,61 +22,68 @@ static void configure(json_value *config)
     if ((value = json_get(config, "disallow-trailing-space", json_boolean)))
         disallow_trailing_space = value->u.boolean;
 
+    if ((value = json_get(config, "require-line-break", json_string)))
+    {
+        line_break = value->u.string.ptr;
+        line_break_length = value->u.string.length;
+    }
+
     if ((value = json_get(config, "require-newline-at-eof", json_boolean)))
         require_newline_at_eof = value->u.boolean;
 }
 
 
-static unsigned get_line_size(unsigned line)
+static unsigned utf8_len(const char *line, unsigned size)
 {
-    if (line + 1 == vec_len(g_lines))
-        return strlen(g_lines[line].start);
-    else
-        return g_lines[line + 1].start - g_lines[line].start - 1;
-}
-
-
-static unsigned get_line_len(unsigned line, unsigned size)
-{
-    unsigned len = 0;
-    char *start = g_lines[line].start;
+    unsigned length = 0;
 
     for (unsigned i = 0; i < size; ++i)
-        if ((start[i] & 0xc0) != 0x80)
-            ++len;
+        if ((line[i] & 0xc0) != 0x80)
+            ++length;
 
-    return len;
+    return length;
 }
 
 
 static void check(void)
 {
-    unsigned line = 0;
-    unsigned size;
+    bool check_lb = !!line_break;
+    unsigned i = 0;
+    const char *line;
+    unsigned length;
 
-    for (; line < vec_len(g_lines); ++line)
+    for (; i < vec_len(g_lines); ++i)
     {
-        if ((size = get_line_size(line)) == 0)
+        line = g_lines[i].start;
+        length = g_lines[i].length;
+
+        if (length == 0)
             continue;
 
-        if (disallow_trailing_space && isspace(g_lines[line].start[size-1]))
+        if (disallow_trailing_space && isspace(line[length - 1]))
         {
-            unsigned column = size - 1;
+            unsigned column = length - 1;
 
-            while (column && isspace(g_lines[line].start[column]))
+            while (column && isspace(line[column]))
                 --column;
 
-            add_warn(line, column + 1, "Trailing witespaces are disallowed");
+            add_warn(i, column + 1, "Trailing witespaces are disallowed");
         }
 
-        if (size > maximum_length && get_line_len(line, size) > maximum_length)
-            add_warn(line, maximum_length,
+        if (length > maximum_length && utf8_len(line, length) > maximum_length)
+            add_warn(i, maximum_length,
                      "Line must be at most %d characters", maximum_length);
+
+        if (check_lb && line[length])
+            if (strncmp(line + length, line_break, line_break_length))
+            {
+                add_warn(i, length, "Invalid line break");
+                check_lb = false;
+            }
     }
 
-    if (require_newline_at_eof)
-        if (size > 0)
-            add_warn(line - 1, size, "Required newline at eof.");
+    if (require_newline_at_eof && g_lines[i - 1].length > 0)
+        add_warn(i - 1, length, "Required newline at eof");
 }
 
 
