cfd7bd9b7bdb74df288f29c6be59d7a19e8352e0
Paul Loyd <pavelko95@gmail.com>
2014-12-09 16:10:44 +0000

    feat: add "allow-before-decls" rule

--- rules/block.c
+++ rules/block.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <string.h>
 
 #include "clint.h"
 
@@ -6,6 +7,7 @@ static bool disallow_empty = false;
 static bool disallow_short = false;
 static bool disallow_oneline = false;
 static bool require_decls_on_top = false;
+static char **allow_before_decls = NULL;
 
 
 #define start_of(tree) g_tokens[tree->start].start
@@ -26,6 +28,19 @@ static void configure(json_value *config)
 
     if ((value = json_get(config, "require-decls-on-top", json_boolean)))
         require_decls_on_top = value->u.boolean;
+
+    if ((value = json_get(config, "allow-before-decls", json_array)))
+    {
+        unsigned len = value->u.array.length;
+        allow_before_decls = new_vec(char *, len);
+
+        for (unsigned i = 0; i < len; ++i)
+        {
+            json_value *item = value->u.array.values[i];
+            if (item->type == json_string)
+                vec_push(allow_before_decls, item->u.string.ptr);
+        }
+    }
 }
 
 
@@ -53,13 +68,31 @@ static void find_oneline(tree_t tree)
 }
 
 
+static bool in_decl_area(tree_t tree)
+{
+    if (tree->type == DECLARATION)
+        return true;
+
+    if (tree->type == CALL)
+    {
+        token_t *name = &g_tokens[tree->start];
+        unsigned len = name->end.pos - name->start.pos + 1;
+
+        for (unsigned i = 0; i < vec_len(allow_before_decls); ++i)
+            if (!strncmp(allow_before_decls[i], name->start.pos, len))
+                return true;
+    }
+
+    return false;
+}
+
+
 static void process_block(struct block_s *tree)
 {
     if (require_decls_on_top)
     {
         unsigned i = 0;
-        while (i < vec_len(tree->entities) &&
-               tree->entities[i]->type == DECLARATION)
+        while (i < vec_len(tree->entities) && in_decl_area(tree->entities[i]))
             ++i;
 
         for (; i < vec_len(tree->entities); ++i)
